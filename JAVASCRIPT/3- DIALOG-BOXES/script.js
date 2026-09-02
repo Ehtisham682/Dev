74f728e2305408b233695152629f3bdc0ecefd88
ehtisham <mohdehtisham682@gmail.com>
2026-09-03 02:02:01 +0530

    dialogbox

---
+++ JAVASCRIPT/3- DIALOG-BOXES/script.js
@@ -0,0 +1,13 @@
+const alertdialog = alert('this is an alert dialog box')
+console.log(alertdialog);
+
+
+const ageCheck =confirm('are you more than 18 years old?')
+console.log(ageCheck)
+// it will return value on the basis of the button clicked by the user.
+// if user clicks on ok it will return true and if clicks on cancel then it will return false.
+
+const name = prompt('kimino namae wa?')
+console.log(name)
+// it will return the value enter by user on text field.
+// if user clicks on cancel then it will return null.
