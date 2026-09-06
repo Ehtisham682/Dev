0014bb1f4793a6d9e47cbd83fd8cdf7f3cfd1a46
ehtisham <mohdehtisham682@gmail.com>
2026-09-06 18:39:51 +0530

    ternary

---
+++ JAVASCRIPT/11- TERNARY OPERATOR/script.js
@@ -0,0 +1,9 @@
+// let gender ='M'
+
+// const message=`${gender.toLocaleUpperCase() === 'M' ? 'He' : 'She'} is a college student`
+// console.log(message)
+
+const msg= null? 'Mohd' : "Ehtisham" ? "Ehti":0
+console.log(msg)
+// this is condition chain."Ehtisham" is in another ternary condition chain.
+// and its True, that's why "Ehti" is returned.
