8aa699560c53f4149987662eda76ca3bf842b4d5
ehtisham <mohdehtisham682@gmail.com>
2026-09-14 21:30:05 +0530

    Array Method- forEach

---
+++ JAVASCRIPT/30- ForEach Array Method/script.js
@@ -0,0 +1,7 @@
+const hardware = ['ram','ssd','cpu','gpu','cooler','monitor']
+
+// hardware.forEach(function(part){
+//     console.log(part)
+// })
+
+hardware.forEach((part)=> console.log(part)) //Arrow function expression
