50c7b5f75e09c035c0dde8b6ad0a637ba908e43c
ehtisham <mohdehtisham682@gmail.com>
2026-09-14 21:28:27 +0530

    function vs method

---
+++ JAVASCRIPT/29- FUNCTIONS VS METHOD/script.js
@@ -0,0 +1,20 @@
+const operation={
+    add: function(a,b){
+        return a+b
+    },
+    sub: function(a,b){
+        return a-b
+    },
+    subtract(a,b){
+        return a-b
+    },
+    div: function(a,b){
+        return a/b
+    },
+    square: function(a){
+        return a**2
+    },
+    cube(a){
+        return a**3
+    }
+}
