4bff9354bfccb25a96aacbcf31cd9320c02285ef
ehtisham <mohdehtisham682@gmail.com>
2026-09-14 21:26:25 +0530

    array functions

---
+++ JAVASCRIPT/29- ARRAY FUNCTIONS/script.js
@@ -0,0 +1,23 @@
+// Array Function Expression
+
+// const multiply = (q,b){
+//     return a*b
+// }
+const multiply = (a,b) => a * b
+
+const add = (a,b) => a+b;
+
+// const square = (num) => num**2;
+const square = num => num**2;
+
+const sub = (a,b) => a-b;
+
+const random = () => Math.random()
+
+// setTimeout( ()=>{
+//     console.log("Hello")
+// },2000)
+
+// setInterval( ()=>{
+//     console.log("Hello")
+// },2000)
