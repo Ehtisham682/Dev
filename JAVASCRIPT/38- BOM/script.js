3d85a639d380fb38ba97d438b4667a8698a3733d
ehtisham <mohdehtisham682@gmail.com>
2026-09-18 21:28:24 +0530

    BOM

---
+++ JAVASCRIPT/38- BOM/script.js
@@ -0,0 +1,22 @@
+/********************************************* Location **************************************/
+// location.href="index.html"
+// location.reload()
+// console.log(location.hostname)
+// console.log(location.pathname)
+// console.log(location)
+
+/********************************************* History **************************************/
+
+//history.forward()
+//history.back()
+//history.go()
+
+/********************************* innerHeight & innerWidth **********************************/
+
+// console.log(innerWidth)
+// console.log(innerHeight)
+
+/********************************* outerHeight & outerWidth **********************************/
+
+// console.log(outerWidth)
+// console.log(outerHeight)
