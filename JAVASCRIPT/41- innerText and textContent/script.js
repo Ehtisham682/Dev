03329f70b06b6c2be0538edfad702f47519384a0
ehtisham <mohdehtisham682@gmail.com>
2026-09-21 04:18:08 +0530

    innerText & textContext

---
+++ JAVASCRIPT/41- innerText and textContent/script.js
@@ -0,0 +1,6 @@
+const para = document.querySelector('p')
+
+console.log(para.innerText) //<b> is display hidder to it wont show in innerText
+// visibility:Hidden also works the same, it will hide in innerText
+
+console.log(para.textContent) // textContent will show the content exactly as written in code. even blank spaces.
