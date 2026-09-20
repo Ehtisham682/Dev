82a29f31d1ac764b9a570a6b59469b923df26fcb
ehtisham <mohdehtisham682@gmail.com>
2026-09-21 04:16:45 +0530

    DOM Intro

---
+++ JAVASCRIPT/39- DOM/script.js
@@ -0,0 +1,11 @@
+// console.dir(document.body.children[0].innerText)
+// console.dir(document.body.children[0].textContent)
+
+// document.body.children[3].children[1].src =
+// 	"https://imgs.search.brave.com/MaKkR6XpQVEzadjdHFmnrwZrAMLeaqhambeNmw4u-_E/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly93MC5w/ZWFrcHguY29tL3dh/bGxwYXBlci81OS83/MDQvSEQtd2FsbHBh/cGVyLWJhbmthaS1z/aGluaWdhbWktaG9s/bG93LWFuaW1lLXF1/aW5jeS1ibGVhY2gt/bWFuZ2EtdGVuc2Et/emFuZ2V0c3UtdGhv/dXNhbmQteWVhci1i/bG9vZC13YXItYXJj/LXN1YnN0aXR1dGUt/c2hpbmlnYW1pLWh1/bWFuLXphbnBha3V0/by1rdXJvc2FraS1p/Y2hpZ28tdGh1bWJu/YWlsLmpwZw";
+
+document.getElementById("img1").src= 'https://i.pinimg.com/1200x/ac/b0/9a/acb09a764a83097a1f515217b93d8964.jpg'
+
+document.getElementById("img2").src='https://i.pinimg.com/736x/df/3b/d8/df3bd89b63389a7d78257a0f694fc0d4.jpg'
+
+document.getElementById("img3").src= 'https://i.pinimg.com/1200x/7b/72/d2/7b72d21b5102feb1718b1fb720cd2c56.jpg'
