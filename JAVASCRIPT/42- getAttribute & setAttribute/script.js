d96284d618668ca34b7ca777c233cfe9b3bb0d18
ehtisham <mohdehtisham682@gmail.com>
2026-09-21 04:18:44 +0530

    getAttribute and setAttribute

---
+++ JAVASCRIPT/42- getAttribute & setAttribute/script.js
@@ -0,0 +1,9 @@
+// const getAtt = document.querySelector('[id]').getAttribute('src')
+const getAtt = document.querySelector('img').getAttribute('src')
+
+
+document.querySelector('h1').setAttribute('id','heading')
+
+
+document.querySelector('h1').className ="newClass"
+document.querySelector('h1').title = "hello there"
